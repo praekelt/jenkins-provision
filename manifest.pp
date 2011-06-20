0c3240f420593a454a15e021b8520285eb0d7f08
Shaun Sephton <shaunsephton@gmail.com>
2011-06-20 12:59:09 +0200

    frist commit

---
+++ manifest.pp
@@ -0,0 +1,18 @@
+exec { "jenkins-key":
+    command => "/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -",
+}
+
+exec { "jenkins-source":
+    command => "/bin/echo \"deb http://pkg.jenkins-ci.org/debian binary/\" > /etc/apt/sources.list.d/jenkins.list",
+    require => Exec["jenkins-key"]
+}
+
+exec { "apt-update":
+    command => "/usr/bin/apt-get update",
+    require => Exec["jenkins-source"]
+}
+
+package { 'jenkins':
+    ensure => installed,
+    require => Exec["apt-update"]
+}
